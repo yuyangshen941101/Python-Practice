e72c9dfee3932bb029d6531ec00eceb2f1c3fd73
yuyangshen941101 <yuyangshen941101@gmail.com>
2025-10-27 12:50:17 +0000

    10/27 更新

--- python字串格式化.py
+++ python字串格式化.py
@@ -4,6 +4,21 @@ P1st = 11.234
 P2nd = -77
 P3th = 34.35
 
-print(f"{P1st:.2f}\n"
-      f"{P2nd:.2f}\n"
-      f"{P3th:.2f}")
+# print(f"{P1st:.2f}\n"
+#       f"{P2nd:.2f}\n"
+#       f"{P3th:.2f}")
+
+# #加上+-號
+# print(f"{P1st:+.2f}\n"
+#       f"{P2nd:+.2f}\n"
+#       f"{P3th:+.2f}")
+
+#對齊 > < ^
+# print(f"{P1st:^10.2f}\n"
+#       f"{P2nd:^10.2f}\n"
+#       f"{P3th:^10.2f}")
+
+# #混和不同符號
+# print(f"{P1st:>+.2f}\n"
+#       f"{P2nd:>+.2f}\n"
+#       f"{P3th:>+.2f}")
---
+++ while_loop迴圈.py
@@ -0,0 +1,16 @@
+# name = ""
+# while name == "":
+#     name = input("請輸入名字:")
+# print(f"您好，{name}")
+
+# food = input("請輸入食物")
+# while food != "q":
+#     print(f"你喜歡的食物是:{food}")
+#     food = input("請輸入食物")
+# print("bye")
+
+# num = int(input("請輸入數字1-10:"))
+# while num > 10 or num < 1 :
+#     print(f"你輸入無效數字")
+#     num = int(input("請輸入數字1-10"))
+# print(f"你輸入的數字是:{num}")
