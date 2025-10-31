20cbc9930ce557443d49a5f724c084123a42087c
yuyangshen941101 <yuyangshen941101@gmail.com>
2025-10-31 16:22:16 +0000

    新增python書本式資料夾

---
+++ for迴圈.py
@@ -0,0 +1,19 @@
+#for迴圈
+# #可迭代的
+
+# for x in range(1, 13):
+#     print(x)
+
+# for x in reversed(range(1, 10)):
+#     print(x)
+# print("happy new years!!")
+
+# card = "1234-5678-9012"
+# for x in card:
+#     if x == "9":
+#         continue
+#     else:
+#         print(x)
+
+#字典
+#鍵KEY 值Vaule 
