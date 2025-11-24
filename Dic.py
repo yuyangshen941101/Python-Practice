f93032707cd5076253df4d6b44a5995c44b6b6ae
yuyangshen9411101 <yuyangshen941101@gmail.com>
2025-11-24 19:02:54 +0800

    更新字典

---
+++ Dic.py
@@ -0,0 +1,24 @@
+captial = {
+    "A" : "1",
+    "B" : "2",
+    "C" : "3"
+}
+
+# get() 取得鍵值對 (A對1)
+# print(captial.get("A"))
+# print(captial.get("B"))
+# print(captial.get("C"))
+
+# update() 更新鍵值對
+# captial.update({"D": "4"})
+# print(captial)
+
+# pop() 刪除鍵值對
+# captial.pop("A")
+# print(captial)
+
+# values() 獲得所有值
+# print(captial.values())
+
+# items() 獲得所有鍵值對
+print(captial.items())
