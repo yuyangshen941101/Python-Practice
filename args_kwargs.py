c977d41d2a639880c669a984589401b0ad7723a1
yuyangshen941101 <yuyangshen941101@gmail.com>
2026-03-08 03:50:06 +0000

    args_kwargs

---
+++ args_kwargs.py
@@ -0,0 +1,24 @@
+# args => arguments 任意數量參數 * => tuple
+# kwargs => 關鍵字 + args (keyword args) ** => dictionary
+
+#args
+
+
+# def add(a,b):
+#     return a + b
+# print(add(1,3))
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         print(f"arg: {arg}")
+#         total += arg
+#     return total
+
+# print(add(3,4))
+
+# def print_info(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"key: {key} value: {value}")
+
+# print_info(name="Alice", age="25", occupation="工程師")
